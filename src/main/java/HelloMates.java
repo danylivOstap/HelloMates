defbf74778c8ee0952ae1f739f7eb9ae4ab0d996
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-01-13 14:48:23 +0200

    Commit 2. Hello mates added

--- src/main/java/HelloMates.java
+++ src/main/java/HelloMates.java
@@ -2,7 +2,8 @@ public class HelloMates {
 
     public static void main(String[] args){
 
-        System.out.println("Hello world");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, mates!)");
 
     }
 }
